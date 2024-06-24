4c18d37f9ebace23815270a9d8dccbd782c45eb3
VoeunChanthou <chanthou.voeun@student.passerellesnumeriques.org>
2024-06-24 09:20:10 +0700

    test code login

--- frontend_g1_rentnow/src/main.ts
+++ frontend_g1_rentnow/src/main.ts
@@ -23,13 +23,13 @@ app.use(router.router)
 app.use(ElementPlus)
 app.use(router.simpleAcl)
 
-import gAuthPlugin from 'vue3-google-oauth2';
-let gauthClientId = '676785461988-icoil0dtlld2fcp5kb22llst7t94mans.apps.googleusercontent.com';
-app.use(gAuthPlugin, {
-  clientId: gauthClientId,
-  scope: 'email',
-  prompt: 'consent',
-})
+// import gAuthPlugin from 'vue3-google-oauth2';
+// let gauthClientId = '676785461988-icoil0dtlld2fcp5kb22llst7t94mans.apps.googleusercontent.com';
+// app.use(gAuthPlugin, {
+//   clientId: gauthClientId,
+//   scope: 'email',
+//   prompt: 'consent',
+// })
 
 app.config.globalProperties.$axios = axios
 
