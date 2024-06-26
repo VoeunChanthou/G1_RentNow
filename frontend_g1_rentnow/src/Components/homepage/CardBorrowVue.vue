ab20946b866013aaf9c3f598d7f8f4f239ed3c64
DAVIT.CHOEUN <DAVIT.CHOEUN@PNC.LAN>
2024-06-26 08:09:16 +0700

    VGP-13 design card for product display

---
+++ frontend_g1_rentnow/src/Components/homepage/CardBorrowVue.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <h1>Borrow The Product</h1>
+    <p>If you want to borrow my product for use, you can follow the instructions.</p>
+    <h4 class="borrow">
+      MotorBike Borrow:
+      <input
+        type="number"
+        class="form-control"
+        placeholder="0"
+        min="0"
+        style="width: 8rem"
+        v-model="cards.motorbike"
+      />
+    </h4>
+    <h4>Price: 1/$</h4>
+    <button type="button" class="btn btn-success" style="background: #f4e065" @click="Borrow()">
+      Order Now
+    </button>
+    <hr />
+    <h4>Total :{{ cards.total + '.00$' }}</h4>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CardBorrowVue',
+  props: ['product'],
+  data() {
+    return {
+      cards: [
+        {
+          motorbike: 0,
+          total: 0
+        }
+      ]
+    }
+  },
+  methods: {
+    Borrow() {
+      if (!this.cards.motorbike) {
+        alert('Please input number')
+        return false
+      } else {
+        this.cards.total = this.cards.motorbike * 15
+      }
+    }
+  }
+}
+</script>
--- frontend_g1_rentnow/src/views/Web/SeviceView.vue
+++ frontend_g1_rentnow/src/views/Web/SeviceView.vue
@@ -4,13 +4,15 @@
 import { Icon } from '@iconify/vue'
 import axiosInstance from '@/plugins/axios'
 import WebLayout from '@/Components/Layouts/WebLayout.vue'
-import { ref } from 'vue'
+import CardVue from '@/Components/homepage/CardVue.vue'
+import { ref, computed } from 'vue'
 import { Search } from '@element-plus/icons-vue'
 import type { componentSizeMap } from 'element-plus'
 import { useAuthStore } from '@/stores/auth-store.ts'
 const AuthUSer = useAuthStore()
 
 const product = ref('')
+const selectedCard = ref('')
 const value = ref('')
 const options = [
   {
@@ -18,36 +20,108 @@ const options = [
     options: [
       {
         value: 'Shanghai',
-        label: 'Shanghai',
+        label: 'Shanghai'
       },
       {
         value: 'Beijing',
-        label: 'Beijing',
-      },
-    ],
+        label: 'Beijing'
+      }
+    ]
   },
   {
     label: 'City name',
     options: [
       {
         value: 'Chengdu',
-        label: 'Chengdu',
+        label: 'Chengdu'
       },
       {
         value: 'Shenzhen',
-        label: 'Shenzhen',
+        label: 'Shenzhen'
       },
       {
         value: 'Guangzhou',
-        label: 'Guangzhou',
+        label: 'Guangzhou'
       },
       {
         value: 'Dalian',
-        label: 'Dalian',
-      },
-    ],
-  },
+        label: 'Dalian'
+      }
+    ]
+  }
 ]
+
+let products = ref([
+  {
+    name: 'High Price Bike',
+    price: 50,
+    image:
+      'https://img.freepik.com/free-photo/portrait-female-teacher-holding-notepad-green_140725-149619.jpg'
+  },
+  {
+    name: 'Simple Price Bike',
+    price: 20,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQazjG1jTiHXRqdd7f4pNP1_K_PzyRuLXNIYw&usqp=CAU'
+  },
+  {
+    name: 'Low Price Bike',
+    price: 10,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQazjG1jTiHXRqdd7f4pNP1_K_PzyRuLXNIYw&usqp=CAU'
+  },
+  {
+    name: 'Low Price Bike',
+    price: 10,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQazjG1jTiHXRqdd7f4pNP1_K_PzyRuLXNIYw&usqp=CAU'
+  },
+  {
+    name: 'Low Price Bike',
+    price: 10,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQazjG1jTiHXRqdd7f4pNP1_K_PzyRuLXNIYw&usqp=CAU'
+  },
+  {
+    name: 'Low Price Bike',
+    price: 10,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQazjG1jTiHXRqdd7f4pNP1_K_PzyRuLXNIYw&usqp=CAU'
+  },
+  {
+    name: 'Low Price Bike',
+    price: 10,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQazjG1jTiHXRqdd7f4pNP1_K_PzyRuLXNIYw&usqp=CAU'
+  },
+  {
+    name: 'Low Price Bike',
+    price: 10,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQazjG1jTiHXRqdd7f4pNP1_K_PzyRuLXNIYw&usqp=CAU'
+  },
+  {
+    name: 'Low Price Bike',
+    price: 10,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQazjG1jTiHXRqdd7f4pNP1_K_PzyRuLXNIYw&usqp=CAU'
+  },
+  {
+    name: 'Low Price Bike',
+    price: 10,
+    image:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQazjG1jTiHXRqdd7f4pNP1_K_PzyRuLXNIYw&usqp=CAU'
+  }
+]);
+const filteredProducts = computed(() => {
+  if (selectedCard.value === 'high_price') {
+    return products.value.filter(product => product.price > 20);
+  } else if (selectedCard.value === 'low_price') {
+    return products.value.filter(product => product.price <= 20);
+  } else {
+    return products.value;
+  }
+});
 
 </script>
 
@@ -81,9 +155,14 @@ const options = [
             <h3 style="color: #e60fa2">Filter</h3>
           </div>
 
-          <div class="select-group" style="margin-top: 20px;">
-            <el-select v-model="value" placeholder="Select catagories" style="width: 100%; margin-bottom: 10px;" size="large">
-              <el-option-group  v-for="group in options" :key="group.label" :label="group.label">
+          <div class="select-group" style="margin-top: 20px">
+            <el-select
+              v-model="value"
+              placeholder="Select catagories"
+              style="width: 100%; margin-bottom: 10px"
+              size="large"
+            >
+              <el-option-group v-for="group in options" :key="group.label" :label="group.label">
                 <el-option
                   v-for="item in group.options"
                   :key="item.value"
@@ -93,9 +172,14 @@ const options = [
               </el-option-group>
             </el-select>
           </div>
-          <div class="select-group" style="margin-top: 20px;">
-            <el-select v-model="value" placeholder="select my shop" style="width: 100%; margin-bottom: 10px;" size="large">
-              <el-option-group  v-for="group in options" :key="group.label" :label="group.label">
+          <div class="select-group" style="margin-top: 20px">
+            <el-select
+              v-model="value"
+              placeholder="select my shop"
+              style="width: 100%; margin-bottom: 10px"
+              size="large"
+            >
+              <el-option-group v-for="group in options" :key="group.label" :label="group.label">
                 <el-option
                   v-for="item in group.options"
                   :key="item.value"
@@ -105,9 +189,14 @@ const options = [
               </el-option-group>
             </el-select>
           </div>
-          <div class="select-group" style="margin-top: 20px;">
-            <el-select v-model="value" placeholder="Day required" style="width: 100%; margin-bottom: 10px;" size="large">
-              <el-option-group  v-for="group in options" :key="group.label" :label="group.label">
+          <div class="select-group" style="margin-top: 20px">
+            <el-select
+              v-model="value"
+              placeholder="Day required"
+              style="width: 100%; margin-bottom: 10px"
+              size="large"
+            >
+              <el-option-group v-for="group in options" :key="group.label" :label="group.label">
                 <el-option
                   v-for="item in group.options"
                   :key="item.value"
@@ -120,13 +209,48 @@ const options = [
         </div>
       </div>
       <div style="height: 100%; display: grid; grid-template-rows: 12% 86%; gap: 20px">
-        <div class="top shadow p-3 rounded bg-white"></div>
-        <div class="top shadow p-3 rounded" style="background: #f4f2f2"></div>
+        <div class="top shadow p-3 rounded bg-white">
+          <div class="selecter">
+            <h2>Result of Product</h2>
+
+            <select
+              id="form-select"
+              style="width: 15rem"
+              v-model="selectedCard"
+              class="form-select"
+              aria-label="Default select example"
+            >
+              <option disabled value="">Please select one price</option>
+              <option value="all_price">All Price</option>
+              <option value="high_price">High Price</option>
+              <option value="low_price">Low Price</option>
+            </select>
+          </div>
+        </div>
+        <div
+          class="top shadow p-3 rounded form_Card"
+          style="background: #f4f2f2; overflow-y: auto"
+          content="width=device-width"
+        >
+          <CardVue v-for="(product, index) in filteredProducts" :key="index" :product="product"></CardVue>
+        </div>
       </div>
     </div>
   </WebLayout>
 </template>
 
 <style>
+.form_Card,
+.selecter {
+  border-radius: 20px;
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.form_Card {
+  flex-wrap: wrap;
+  width: 100%;
+}
 </style>
 
