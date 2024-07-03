c256dc75c7552f7dd24a6496a7468eb6646083f0
VANNY.CHAN <VANNY.CHAN@PNC.LAN>
2024-07-03 10:19:48 +0700

    redesign on home page

--- frontend_g1_rentnow/src/views/Web/HomeView.vue
+++ frontend_g1_rentnow/src/views/Web/HomeView.vue
@@ -3,19 +3,20 @@
 import axiosInstance from '@/plugins/axios'
 import WebLayout from '@/Components/Layouts/WebLayout.vue'
 import CategoryShow from '@/Components/homepage/CategoryShow.vue'
-import WebLoginVue from '@/Components/homepage/WebLogin.vue';
-import PopupRegisterVue from '@/Components/homepage/PopupRegister.vue';
-import type { componentSizeMap } from 'element-plus';
-import {useAuthStore} from '@/stores/auth-store.ts'
+import WebLoginVue from '@/Components/homepage/WebLogin.vue'
+import PopupRegisterVue from '@/Components/homepage/PopupRegister.vue'
+import type { componentSizeMap } from 'element-plus'
+import { useAuthStore } from '@/stores/auth-store.ts'
 import PopularShopVue from '@/Components/homepage/PopularShop.vue'
 import VideoWork from '@/Components/homepage/VideoWork.vue'
 import PeopleSay from '@/Components/homepage/PeopleSay.vue'
+import ProductShow from '@/Components/homepage/ProductShow.vue'
 const AuthUSer = useAuthStore()
 </script>
 
 <template>
   <WebLayout>
-    <div style="height: 70vh; display: flex">
+    <div style="height: 70vh; display: flex; background-color:#F4F3EF ;">
       <div
         class="head-right px-5"
         style="
@@ -72,32 +73,38 @@ const AuthUSer = useAuthStore()
       <div class="head-left" style="height: 100%; width: 50%; display: flex; align-items: center">
         <img src="../../assets/image-removebg-preview.png" alt="" style="width: 70%" />
       </div>
-      </div>
-      
-  <!-- register popup  -->
-  <PopupRegisterVue></PopupRegisterVue>
-    
-  
-  <!-- login form -->
-  <WebLoginVue></WebLoginVue>
-  
-  <!-- category show -->
-  <CategoryShow></CategoryShow>
-  
-  <!-- PopularShop -->
-  <PopularShopVue></PopularShopVue>
-  
-  <!-- VideoWork -->
-  <VideoWork></VideoWork>
-  
-  <!-- PeopleSay -->
-  <PeopleSay></PeopleSay>
-  
-</WebLayout>
+    </div>
+
+    <div class="components">
+    <!-- register popup  -->
+    <PopupRegisterVue></PopupRegisterVue>
+
+    <!-- login form -->
+    <WebLoginVue></WebLoginVue>
+
+    <!-- category show -->
+    <CategoryShow></CategoryShow>
+
+    <!-- ProductShow -->
+    <ProductShow></ProductShow>
+
+    <!-- PopularShop -->
+    <PopularShopVue></PopularShopVue>
+
+    <!-- VideoWork -->
+    <VideoWork></VideoWork>
+
+    <!-- PeopleSay -->
+    <PeopleSay></PeopleSay>
+  </div>
+  </WebLayout>
 </template>
 
 <style>
-
+.components{
+  background: white;
+  padding: 30px;
+}
 </style>
 
 
